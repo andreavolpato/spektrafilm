21fac0b9309a6e34bc6da13fcba3e7ca7a3ed10f
andreavolpato <andrea.volpato@outlook.com>
2026-03-16 08:35:13 +0100

    refactor: old tests to examples

--- examples/midgray_balance.py
+++ examples/midgray_balance.py
@@ -1,3 +1,10 @@
+"""
+Example: Check midgray balance of a film profile.
+
+This script loads a profile and computes the integrated sensitivity
+under a D55 illuminant to check the balance of the film layers.
+"""
+
 import numpy as np
 from agx_emulsion.profiles.io import load_profile
 from agx_emulsion.model.illuminants import standard_illuminant
@@ -5,4 +12,4 @@ from agx_emulsion.model.illuminants import standard_illuminant
 p = load_profile('kodak_portra_400_au')
 ill = standard_illuminant(type='D55')
 s = 10**np.double(p.data.log_sensitivity)
-print(np.nansum(ill[:,None]*s, axis=0))
+print(np.nansum(ill[:, None] * s, axis=0))
